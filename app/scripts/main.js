6117a23c631fda93bdc635ff2ae47c94996dd6e9
Anand Thakker <vestibule@anandthakker.net>
2015-05-20 20:18:27 -0400

    build: add es6 and jsx transformation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('Heyo!');
+var React = require('react');
+
+class Hello extends React.Component {
+  render () {
+    return <div>Hello, {this.props.name}!</div>;
+  }
+}
+
+Hello.displayName = 'Hello';
+Hello.propTypes = {
+  name: React.PropTypes.string.isRequired
+};
+
+React.render(<Hello name='fellow earthling'/>, document.body);
