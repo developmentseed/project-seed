817e207607c0303189d291121db304f4dddb02a0
Vitor George <vitor.george@gmail.com>
2021-05-11 09:13:40 +0100

    A minimal example of how to use sql.js-httpvfs in project seed

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -1,22 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { render } from 'react-dom';
 
 import GlobalStyles from './styles/global';
 
+import { createDbWorker } from 'sql.js-httpvfs';
+
 // Root component.
 function Root() {
+  const [text, setText] = useState(null);
+
   useEffect(() => {
     // Hide the welcome banner.
     const banner = document.querySelector('#welcome-banner');
     banner.classList.add('dismissed');
     setTimeout(() => banner.remove(), 500);
+
+    // Create SQLite worked
+    createDbWorker(
+      [
+        {
+          from: 'inline',
+          config: {
+            serverMode: 'full',
+            url: '/example.sqlite3',
+            requestChunkSize: 4096
+          }
+        }
+      ],
+      './sqlite.worker.js',
+      './sql-wasm.wasm'
+    ).then((worker) => {
+      // Do a query
+      worker.db
+        .query(`select * from mytable`)
+        .then((result) => setText(JSON.stringify(result)));
+    });
   }, []);
 
   return (
     <ThemeProvider theme={{}}>
       <GlobalStyles />
-      <p>Hello from Starter</p>
+      <p>{text || 'Loading...'}</p>
     </ThemeProvider>
   );
 }
